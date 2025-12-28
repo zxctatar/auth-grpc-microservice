ba8ee9e68e65358775326f8e53212b82c2435d3d
ashlyaud <maksimknutov90@gmail.com>
2025-12-28 23:04:49 +0300

    added validate data in regmodel

--- internal/usecase/registration/regmodel.go
+++ internal/usecase/registration/regmodel.go
@@ -1,5 +1,14 @@
 package registration
 
+import "errors"
+
+var (
+	ErrEmptyFirstName = errors.New("empty first name")
+	ErrEmptyLastName  = errors.New("empty last name")
+	ErrEmptyPassword  = errors.New("empty password")
+	ErrEmptyEmail     = errors.New("empty email")
+)
+
 type RegInput struct {
 	FirstName  string
 	MiddleName string
@@ -8,12 +17,34 @@ type RegInput struct {
 	Email      string
 }
 
-func NewRegInput(firstName, middleName, lastName, password, email string) *RegInput {
+func NewRegInput(firstName, middleName, lastName, password, email string) (*RegInput, error) {
+	err := validateRegInput(firstName, lastName, password, email)
+
+	if err != nil {
+		return nil, err
+	}
+
 	return &RegInput{
 		FirstName:  firstName,
 		MiddleName: middleName,
 		LastName:   lastName,
 		Password:   password,
 		Email:      email,
+	}, nil
+}
+
+func validateRegInput(firstName, lastName, password, email string) error {
+	if firstName == "" {
+		return ErrEmptyFirstName
 	}
+	if lastName == "" {
+		return ErrEmptyLastName
+	}
+	if password == "" {
+		return ErrEmptyPassword
+	}
+	if email == "" {
+		return ErrEmptyEmail
+	}
+	return nil
 }
