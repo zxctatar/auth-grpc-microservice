c274a02b59d3e0febcd8e699ded246a0b8d3bb83
ashlyaud <maksimknutov.w80@gmail.com>
2025-12-28 23:54:02 +0300

    use gofmt

--- internal/domain/user/error.go
+++ internal/domain/user/error.go
@@ -3,6 +3,6 @@ package userdomain
 import "errors"
 
 var (
-	ErrInvalidEmail = errors.New("invalid email address")
+	ErrInvalidEmail    = errors.New("invalid email address")
 	ErrInvalidPassword = errors.New("invalid password")
 )
