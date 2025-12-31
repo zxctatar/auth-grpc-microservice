a3daaaf683bd90215d2a3f0b3e14d038af8db21a
ashlyaud <maksimknutov90@gmail.com>
2025-12-31 15:10:28 +0300

    The FindByEmail method has been implemented, errors have been added to the repository

--- internal/infrastructure/postgres/postgres.go
+++ internal/infrastructure/postgres/postgres.go
@@ -2,8 +2,10 @@ package postgres
 
 import (
 	userdomain "auth/internal/domain/user"
+	"auth/internal/repository"
 	"context"
 	"database/sql"
+	"errors"
 	"log/slog"
 )
 
@@ -24,5 +26,37 @@ func (p *Postgres) Save(ctx context.Context, user *userdomain.UserDomain) (uint3
 }
 
 func (p *Postgres) FindByEmail(ctx context.Context, email string) (*userdomain.UserDomain, error) {
-	panic("not implemented")
+	const op = "postgres.FindByEmail"
+
+	log := p.log.With(slog.String("op", op), slog.String("email", email))
+
+	log.Info("start find user by email")
+
+	row := p.db.QueryRowContext(ctx, "SELECT id, first_name, middle_name, last_name, hash_password, email FROM users WHERE email = $1", email)
+
+	var posModel PostgresModel
+
+	err := row.Scan(
+		&posModel.Id,
+		&posModel.FirstName,
+		&posModel.MiddleName,
+		&posModel.LastName,
+		&posModel.HashPassword,
+		&posModel.Email,
+	)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			log.Info("unsuccessful user search by email", slog.String("error", err.Error()))
+			return nil, repository.ErrUserNotFound
+		}
+		log.Error("error searching for user by email", slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	userOut := modelToDomain(&posModel)
+
+	p.log.Info("user found")
+
+	return userOut, nil
 }
