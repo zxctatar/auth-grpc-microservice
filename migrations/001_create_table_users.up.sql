5e55baad679747a7c1c56def591e02286d38c30f
ashlyaud <maksimknutov90@gmail.com>
2025-12-31 05:07:08 +0300

    fix migration

--- migrations/001_create_table_users.up.sql
+++ migrations/001_create_table_users.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users(
     middle_name VARCHAR(255),
     last_name VARCHAR(255) NOT NULL,
     hash_password TEXT NOT NULL,
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE INDEX IF NOT EXISTS idx_email ON users(email);
