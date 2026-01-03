8b2c84dbdf7b5880a2a6cf370d5b5dcd86f94c8e
ashlyaud <maksimknutov.w80@gmail.com>
2026-01-03 17:55:25 +0300

    add folder for repo

---
+++ internal/repository/storagerepo/error.go
@@ -0,0 +1,7 @@
+package storagerepo
+
+import "errors"
+
+var (
+	ErrUserNotFound = errors.New("user not found")
+)
---
+++ internal/repository/storagerepo/repo.go
@@ -0,0 +1,13 @@
+package storagerepo
+
+import (
+	userdomain "auth/internal/domain/user"
+	logmodel "auth/internal/usecase/models/login"
+	"context"
+)
+
+type StorageRepo interface {
+	Save(ctx context.Context, user *userdomain.UserDomain) (uint32, error)
+	FindByEmail(ctx context.Context, email string) (*userdomain.UserDomain, error)
+	FindAuthDataByEmail(ctx context.Context, email string) (*logmodel.UserAuthData, error)
+}
